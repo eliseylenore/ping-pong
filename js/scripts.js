5fad77d309e72a221c997dfa0e86e73b18b02d50
Elise St Hilaire <eliseylenore@gmail.com>
2017-01-20 10:27:11 -0800

    edit spacing issues

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,10 @@ var pingPongCount = function(userInput) {
   var userNumber = parseInt(userInput);
   var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-  //count from 1-userNumber
+  //function to count from 1-userNumber
   var countUp = function(userInput) {
     var outputNumber = '';
-    for(i = 1; i < userNumber ; i++) {
+    for (i = 1; i < userNumber ; i++) {
       console.log(i);
       if (i % 15 === 0) {
         outputNumber += "ping-pong" + " ";
@@ -23,7 +23,6 @@ var pingPongCount = function(userInput) {
     return outputNumber;
   }
   //determines if input can be counted to
-  var numQuery = numbers.indexOf(numbers);
   if (userInput.search(/^[0-9]+$/)|| userNumber < 1) {
     return "I'm sorry, we weren't able to count up to that number. Please make sure that you are entering a number and it is more than zero. Also, please type the digit (3) instead of the word (three)."
   } else {
