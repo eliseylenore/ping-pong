43b8cc8a7136d92e30eb52fa4c83489b1b8cdab1
Elise St Hilaire <eliseylenore@gmail.com>
2017-01-20 09:13:48 -0800

    initialize backend logic, alert user when input cannot be ping-ponged

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
 //back-end
 
+var pingPongCount = function(userInput) {
+  var userNumber = parseInt(userInput);
+  var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+
+  //determines if input can be counted to
+  var numQuery = numbers.indexOf(numbers);
+  if (userInput.search(/^[0-9]+$/)|| userNumber < 1) {
+    return "I'm sorry, we weren't able to count up to that number. Please make sure that you are entering a number and it is more than zero. Also, please type the digit (3) instead of the word (three)."
+  }
+}
+
 
 
 
